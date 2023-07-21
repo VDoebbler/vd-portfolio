342d1fb8cb8158bd2a84f7b86f0b8689e0bf5ad4
VDoebbler <victor.doebbler@gmail.com>
2023-07-21 15:33:35 -0400

    Moved app structure back to the way it was before. Finally got migrations for nearbyshops to work

--- nearbyshops/settings.py
+++ nearbyshops/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--shpx4xbzj1h45(0+^6n4_h=j+dv*d)ni4s@pp+p=h8*y6cz98'
+SECRET_KEY = 'django-insecure-z*mn9&n%4)(*15bsw(gshc2r&@drro_fflf^67g$=p=%@^gfls'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'projects',
     'blog',
     'home',
+    'nearbyshops',
 ]
 
 MIDDLEWARE = [
